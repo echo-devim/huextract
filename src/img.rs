00eb3789422328372912f214b6de6d1081c50cee
Romain Garbage <romain@rgarbage.fr>
2022-09-05 10:39:33 +0200

    Extend Img struct to store padding information

--- src/img.rs
+++ src/img.rs
@@ -6,16 +6,25 @@ use crate::img_header::ImgHeader;
 pub struct Img {
     pub header: ImgHeader,
     pub offset: u64,
+    pub padding: u64,
 }
 
 impl Img {
-    pub fn new(header: ImgHeader, offset: u64) -> Self {
-        Self { header, offset }
+    pub fn new(header: ImgHeader, offset: u64, padding: u64) -> Self {
+        Self {
+            header,
+            offset,
+            padding,
+        }
     }
 }
 
 impl std::fmt::Display for Img {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Header:\n{}\nOffset: {}", self.header, self.offset)
+        write!(
+            f,
+            "Header:\n{}\nOffset: {}\nPadding: {}",
+            self.header, self.offset, self.padding
+        )
     }
 }
