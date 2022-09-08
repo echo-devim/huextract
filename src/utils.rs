39b6dd1025e29d6691b2c9afa9e94d029fab7746
Romain Garbage <romain@rgarbage.fr>
2022-09-08 23:41:38 +0200

    Modify remove_null_bytes() to work with slices

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,14 @@
 //! This module contains utility functions
 //!
 
-pub fn remove_null_bytes(buffer: [u8; 16]) -> Vec<u8> {
-    let mut pos = 15;
-    while buffer[pos] == 0x00 && pos != 0 {
-        pos -= 1;
+pub fn remove_null_bytes(buffer: &[u8]) -> Vec<u8> {
+    if buffer.is_empty() {
+        Vec::new()
+    } else {
+        let mut pos = buffer.len() - 1;
+        while buffer[pos] == 0x00 && pos != 0 {
+            pos -= 1;
+        }
+        Vec::from(&buffer[..=pos])
     }
-    Vec::from(&buffer[..=pos])
 }
